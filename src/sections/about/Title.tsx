a9d7b6aaa837a1ef1690f8bcdc9fa15999558d9a
iskana-fardan <devfard@gmail.com>
2026-01-20 17:33:57 +0200

    feat (About): add button for back to home at title component

--- src/sections/about/Title.tsx
+++ src/sections/about/Title.tsx
@@ -1,15 +1,42 @@
-import { Stack, Typography } from '@mui/material'
+import { IconButton, Stack, Typography } from '@mui/material'
+import ArrowBackRoundedIcon from '@mui/icons-material/ArrowBackRounded';
+import { NavLink } from 'react-router-dom';
 
 const Title = () => {
   return (
-    <Stack spacing={2} sx={{mb:6}}>
-        <Typography variant="h4" fontWeight={600} color="text.primary"> 
-            About Islamic Learning Roadmaps
-        </Typography>
-        <Typography color="text.secondary" fontSize={"1.2rem"}>
-            A structured guide to navigating the traditional Islamic curriculum
-        </Typography>
+    <>
+    <Stack spacing={3} mt={11} mx={1} py={2}>
+        {/* Back */}
+        <IconButton
+          disableRipple
+          sx={{ 
+            alignSelf: "flex-start", 
+            alignItems: "center",
+            fontSize: "18px", 
+            borderRadius: "5px",
+            "&:hover": {
+                backgroundColor: "rgba(100,100,100,0.1)",
+                color:"text.primary"
+            },
+          }}
+          component={NavLink}
+          to={"/"}
+        >
+          <ArrowBackRoundedIcon fontSize='inherit' />
+          <Typography ml={1.2} fontSize= "0.75rem">
+            Back to Home
+          </Typography>
+        </IconButton>
     </Stack>
+    <Stack spacing={2} sx={{mb:6}}>
+          <Typography variant="h4" fontWeight={600} color="text.primary"> 
+              About Islamic Learning Roadmaps
+          </Typography>
+          <Typography color="text.secondary" fontSize={"1.2rem"}>
+              A structured guide to navigating the traditional Islamic curriculum
+          </Typography>
+    </Stack>
+    </>
   )
 }
 
